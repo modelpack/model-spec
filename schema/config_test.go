dfe771f622ea9d24f9deb3ddb0c7263ba29da8c7
caozhuozi <543481992@qq.com>
2025-04-12 12:33:29 +0800

    add json schema & validator for config
    Signed-off-by: caozhuozi <543481992@qq.com>

---
+++ schema/config_test.go
@@ -0,0 +1,315 @@
+/*
+ *     Copyright 2025 The CNAI Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package schema_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/CloudNativeAI/model-spec/schema"
+)
+
+func TestConfig(t *testing.T) {
+	for i, tt := range []struct {
+		config string
+		fail   bool
+	}{
+		// expected failure: config is missing
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: version is a number
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": 3.1
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: revision is a number
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1",
+    "revision": 1234567890
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: createdAt is not RFC3339 format
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1",
+    "createdAt": "2025/01/01T00:00:00Z"
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: authors is not an array
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1",
+	"authors": "John Doe"
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: licenses is not an array
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1",
+	"licenses": "Apache-2.0"
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: docURL is an array
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1",
+    "docURL": [
+       "https://example.com/doc"
+    ]
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: sourceURL is an array
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1",
+    "sourceURL": [
+       "https://github.com/xyz/xyz3"
+    ]
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: paramSize is a number
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1"
+  },
+  "config": {
+     "paramSize": 8000000
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: precision is a number
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1"
+  },
+  "config": {
+     "precision": 16
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: type is not "layers"
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1"
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layer",
+    "diff_ids": [
+       "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+    ]
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: diff_ids is not an array
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1"
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": "sha256:1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+  }
+}
+`,
+			fail: true,
+		},
+		// expected failure: diff_ids is empty
+		{
+			config: `
+{
+  "descriptor": {
+    "name": "xyz-3-8B-Instruct",
+    "version": "3.1"
+  },
+  "config": {
+     "paramSize": "8b"
+  },
+  "modelfs": {
+    "type": "layers",
+    "diff_ids": []
+  }
+}
+`,
+			fail: true,
+		},
+	} {
+		r := strings.NewReader(tt.config)
+		err := schema.ValidatorMediaTypeModelConfig.Validate(r)
+
+		if got := err != nil; tt.fail != got {
+			t.Errorf("test %d: expected validation failure %t but got %t, err %v", i, tt.fail, got, err)
+		}
+	}
+}
---
+++ schema/validator.go
@@ -0,0 +1,125 @@
+/*
+ *     Copyright 2025 The CNAI Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package schema
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	v1 "github.com/CloudNativeAI/model-spec/specs-go/v1"
+	"github.com/santhosh-tekuri/jsonschema/v5"
+)
+
+// Validator wraps a media type string identifier and implements validation against a JSON schema.
+type Validator string
+
+// Validate validates the given reader against the schema of the wrapped media type.
+func (v Validator) Validate(src io.Reader) error {
+	// run the media type specific validation
+	if fn, ok := validateByMediaType[v]; ok {
+		if fn == nil {
+			return fmt.Errorf("internal error: mapValidate is nil for %s", string(v))
+		}
+		// buffer the src so the media type validation and the schema validation can both read it
+		buf, err := io.ReadAll(src)
+		if err != nil {
+			return fmt.Errorf("failed to read input: %w", err)
+		}
+		src = bytes.NewReader(buf)
+		err = fn(buf)
+		if err != nil {
+			return err
+		}
+	}
+
+	// json schema validation
+	return v.validateSchema(src)
+}
+
+func (v Validator) validateSchema(src io.Reader) error {
+	if _, ok := specs[v]; !ok {
+		return fmt.Errorf("no validator available for %s", string(v))
+	}
+
+	c := jsonschema.NewCompiler()
+
+	// load the schema files from the embedded FS
+	dir, err := specFS.ReadDir(".")
+	if err != nil {
+		return fmt.Errorf("spec embedded directory could not be loaded: %w", err)
+	}
+	for _, file := range dir {
+		if file.IsDir() {
+			continue
+		}
+		specBuf, err := specFS.ReadFile(file.Name())
+		if err != nil {
+			return fmt.Errorf("could not read spec file %s: %w", file.Name(), err)
+		}
+		err = c.AddResource(file.Name(), bytes.NewReader(specBuf))
+		if err != nil {
+			return fmt.Errorf("failed to add spec file %s: %w", file.Name(), err)
+		}
+		if len(specURLs[file.Name()]) == 0 {
+			// this would be a bug in the validation code itself, add any missing entry to schema.go
+			return fmt.Errorf("spec file has no aliases: %s", file.Name())
+		}
+		for _, specURL := range specURLs[file.Name()] {
+			err = c.AddResource(specURL, bytes.NewReader(specBuf))
+			if err != nil {
+				return fmt.Errorf("failed to add spec file %s as url %s: %w", file.Name(), specURL, err)
+			}
+		}
+	}
+
+	// compile based on the type of validator
+	schema, err := c.Compile(specs[v])
+	if err != nil {
+		return fmt.Errorf("failed to compile schema %s: %w", string(v), err)
+	}
+
+	// read in the user input and validate
+	var input interface{}
+	err = json.NewDecoder(src).Decode(&input)
+	if err != nil {
+		return fmt.Errorf("unable to parse json to validate: %w", err)
+	}
+	err = schema.Validate(input)
+	if err != nil {
+		return fmt.Errorf("validation failed: %w", err)
+	}
+	return nil
+}
+
+type validateFunc func([]byte) error
+
+var validateByMediaType = map[Validator]validateFunc{
+	ValidatorMediaTypeModelConfig: validateConfig,
+}
+
+func validateConfig(buf []byte) error {
+	mc := v1.ModelConfig{}
+
+	err := json.Unmarshal(buf, &mc)
+	if err != nil {
+		return fmt.Errorf("config format mismatch: %w", err)
+	}
+
+	return nil
+}
